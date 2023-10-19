45480214a006f30419e74c2dde321839f8eefd97
jmedina-data <136740683+jmedina-data@users.noreply.github.com>
2023-10-19 17:48:22 +0000

    challenge 3

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,18 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+-- using logic and equality operators
+
+SELECT Name, Description, Type, Price
+FROM Dishes
+ORDER BY Price;
+
+SELECT Name, Type
+FROM Dishes
+WHERE Type = "Appetizer" OR Type = "Beverage" ;
+
+SELECT Name, Description, Type, Price
+FROM Dishes
+WHERE Type != "Beverage"
+ORDER BY Type;
