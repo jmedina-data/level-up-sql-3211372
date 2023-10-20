43f4417fc0bc6ed103997f383d409f032c72f49b
jmedina-data <136740683+jmedina-data@users.noreply.github.com>
2023-10-20 16:15:20 +0000

    challenge 5

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,19 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+
+-- find customer
+SELECT CustomerID, FirstName, LastName, Address
+FROM Customers
+WHERE FirstName = 'Taylor'
+AND LastName = 'Jenkins';
+-- update fields in table
+UPDATE Customers
+SET Address = '74 Pine St.',
+    City = 'New York',
+    State = 'NY'
+WHERE CustomerID = 26;
+-- now check
+SELECT *
+FROM Customers
+WHERE CustomerID = 26;
